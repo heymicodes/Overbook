4c090f7d8fde717f4c4fe95d6c70936263957835
Heymi <heymicodes@gmail.com>
2020-08-16 20:40:49 +0200

    Add relations with Author and Category tables

--- app/Book.php
+++ app/Book.php
@@ -14,4 +14,18 @@ class Book extends Model
     protected $fillable = [
         'title', 'summary', 'cover', 'ISBN', 'publication_date', 'score',
     ];
+
+    public function authors()
+    {
+        return $this->belongsToMany('App\Author', 'book_author', 'book_id', 'author_id')
+                        ->using('App\BookAuthor')
+                        ->withTimestamps();
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category', 'book_category', 'book_id', 'category_id')
+                        ->using('App\BookCategory')
+                        ->withTimestamps();
+    }
 }
