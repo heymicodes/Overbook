240cd55cea29cb42c7cabd487319146681958140
Heymi <heymicodes@gmail.com>
2020-08-16 19:17:05 +0200

    Remove 'role' from mass assignable attributes

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-       'lastname', 'firstname', 'username', 'email', 'password', 'avatar', 'role',
+       'lastname', 'firstname', 'username', 'email', 'password', 'avatar',
     ];
 
     /**
